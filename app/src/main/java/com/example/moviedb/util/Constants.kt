f12ad0539e8febddda90a6a17554cfbd051bb068
jemish54 <79212500+jemish54@users.noreply.github.com>
2022-07-06 22:39:13 +0530

    Update Constants.kt

--- app/src/main/java/com/example/moviedb/util/Constants.kt
+++ app/src/main/java/com/example/moviedb/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.moviedb.util
 class Constants {
 
     companion object{
-        const val API_KEY = "ee88d18a175dcbee84e618f9ccfc48a5"
+        const val API_KEY = "//ATTACH YOUR API KEY//"
         const val BASE_URL = "https://api.themoviedb.org/3/movie/"
         const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500"
     }
