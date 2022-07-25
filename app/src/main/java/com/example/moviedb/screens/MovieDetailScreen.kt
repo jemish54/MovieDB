c92bb3561a1c970cbe09685e707651f7bce904ad
jemish54 <jemishmavani054@gmail.com>
2022-07-25 15:18:32 +0530

    DetailScreen Improvements

--- app/src/main/java/com/example/moviedb/screens/MovieDetailScreen.kt
+++ app/src/main/java/com/example/moviedb/screens/MovieDetailScreen.kt
@@ -2,14 +2,11 @@ package com.example.moviedb.screens
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -31,119 +28,127 @@ import com.example.moviedb.models.Movie
 import com.example.moviedb.models.WatchItem
 import com.example.moviedb.ui.theme.MovieDBTheme
 import com.example.moviedb.util.Constants.Companion.IMAGE_BASE
+import com.example.moviedb.util.genreDB
 import com.example.moviedb.viewmodels.MainViewModel
 
 @Composable
 fun MovieDetailScreen(viewModel: MainViewModel, navController: NavController) {
     val movie: Movie? = viewModel.movieDetail.collectAsState().value
-    Scaffold(modifier = Modifier.fillMaxSize()) {
-        movie?.let {
-            DetailView(it, navController, viewModel)
-        }
+    movie?.let {
+        DetailView(it, navController, viewModel)
     }
+
 }
 
 @Composable
 fun DetailView(movie: Movie, navController: NavController, viewModel: MainViewModel) {
 
-    var inWatchList by remember {
-        mutableStateOf(movie.in_watchlist)
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(0.7f),
-        ) {
-            movie.poster_path?.let {
-                AsyncImage(
-                    model = "$IMAGE_BASE${it}",
-                    contentDescription = "Movie Poster",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop,
-                    alignment = Alignment.Center
-                )
-            } ?: Box(
-                modifier = Modifier.fillMaxSize().align(Alignment.Center)
-            ){
-                Text("No Poster Available", color = MaterialTheme.colors.onSurface)
-            }
-
-            IconButton(
-                onClick = {
-                    navController.popBackStack()
-                },
-                modifier = Modifier.padding(12.dp)
-            ) {
-                Box(
-                    modifier = Modifier.background(
-                        color = Color.Red.copy(alpha = 0.5f),
-                        shape = RoundedCornerShape(6.dp),
-                    )
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back Arrow",
-                        modifier = Modifier.padding(6.dp),
-                        tint = Color.White
-                    )
-                }
-            }
+    Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        var inWatchList by remember {
+            mutableStateOf(movie.in_watchlist)
         }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                ChipText(
-                    text = "18+",
-                )
-                ChipText(text = "Action")
-                ChipText(text = "${movie.vote_average}", icon = Icons.Filled.Star)
-            }
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                IconButton(onClick = {
-                    if(inWatchList){
-                        viewModel.removeWatchItem(movie.id)
-                    }else{
-                        viewModel.insertWatchItem(
-                            WatchItem(
-                                itemId = movie.id,
-                                type = true,
-                                movie = movie
-                            )
-                        )
-                    }
-                    movie.in_watchlist = !movie.in_watchlist
-                    inWatchList = !inWatchList
-                }) {
-                    Icon(
-                        imageVector = if(inWatchList) Icons.Default.Delete else Icons.Default.AddCircle,
-                        contentDescription = "Add to WatchList"
+        FloatingActionButton(backgroundColor = Color.Red,onClick = {
+            if (inWatchList) {
+                viewModel.removeWatchItem(movie.id)
+            } else {
+                viewModel.insertWatchItem(
+                    WatchItem(
+                        itemId = movie.id,
+                        type = true,
+                        movie = movie
                     )
-                }
+                )
             }
+            movie.in_watchlist = !movie.in_watchlist
+            inWatchList = !inWatchList
+        }) {
+            Icon(
+                imageVector = if (inWatchList) Icons.Default.Delete else Icons.Default.Add,
+                contentDescription = "Add to WatchList",
+                modifier = Modifier.size(32.dp),
+                tint = MaterialTheme.colors.surface
+            )
         }
+    }) {
 
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
         ) {
-            Text(text = movie.title, fontSize = 22.sp)
-            Spacer(modifier = Modifier.height(12.dp))
-            ExpandableText(text = movie.overview)
-        }
-    }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(0.7f),
+            ) {
+                movie.poster_path?.let {
+                    AsyncImage(
+                        model = "$IMAGE_BASE${it}",
+                        contentDescription = "Movie Poster",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.Center
+                    )
+                } ?: Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center)
+                ) {
+                    Text("No Poster Available", color = MaterialTheme.colors.onSurface)
+                }
 
+                IconButton(
+                    onClick = {
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.padding(12.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.size(48.dp).background(
+                            color = Color.Red,
+                            shape = RoundedCornerShape(6.dp),
+                        ), contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back Arrow",
+                            modifier = Modifier.padding(6.dp),
+                            tint = MaterialTheme.colors.surface
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(20.dp)
+                ) {
+                    ChipText(text = "${movie.vote_average}", icon = Icons.Filled.Star)
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .horizontalScroll(rememberScrollState()),
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                movie.genre_ids.forEach {
+                    ChipText(text = "${genreDB[it]}")
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                Text(text = movie.title, fontSize = 22.sp)
+                Spacer(modifier = Modifier.height(12.dp))
+                ExpandableText(text = movie.overview)
+            }
+        }
+
+    }
 }
 
 @Composable
@@ -195,8 +200,6 @@ fun ChipText(
         }
     }
 }
-
-
 
 
 
--- app/src/main/java/com/example/moviedb/screens/SeriesDetailScreen.kt
+++ app/src/main/java/com/example/moviedb/screens/SeriesDetailScreen.kt
@@ -2,18 +2,12 @@ package com.example.moviedb.screens
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,10 +25,11 @@ import com.example.moviedb.models.Movie
 import com.example.moviedb.models.Series
 import com.example.moviedb.models.WatchItem
 import com.example.moviedb.util.Constants
+import com.example.moviedb.util.genreDB
 import com.example.moviedb.viewmodels.MainViewModel
 
 @Composable
-fun SeriesDetailScreen(viewModel: MainViewModel,navController: NavController){
+fun SeriesDetailScreen(viewModel: MainViewModel, navController: NavController) {
     val series = viewModel.seriesDetail.collectAsState().value
     Scaffold() {
         series?.let {
@@ -46,100 +41,108 @@ fun SeriesDetailScreen(viewModel: MainViewModel,navController: NavController){
 @Composable
 fun DetailView(series: Series, navController: NavController, viewModel: MainViewModel) {
 
-    var inWatchList by remember {
-        mutableStateOf(series.in_watchlist)
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(0.7f),
-        ) {
-            series.poster_path?.let {
-                AsyncImage(
-                    model = "${Constants.IMAGE_BASE}${it}",
-                    contentDescription = "Movie Poster",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop,
-                    alignment = Alignment.Center
-                )
-            } ?: Box(
-                modifier = Modifier.fillMaxSize().align(Alignment.Center)
-            ){
-                Text("No Poster Available")
-            }
-            IconButton(
-                onClick = {
-                    navController.popBackStack()
-                },
-                modifier = Modifier.padding(12.dp)
-            ) {
-                Box(
-                    modifier = Modifier.background(
-                        color = Color.Red.copy(alpha = 0.5f),
-                        shape = RoundedCornerShape(6.dp),
-                    )
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back Arrow",
-                        modifier = Modifier.padding(6.dp),
-                        tint = Color.White
-                    )
-                }
-            }
+    Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        var inWatchList by remember {
+            mutableStateOf(series.in_watchlist)
         }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                ChipText(
-                    text = "18+",
-                )
-                ChipText(text = "Action")
-                ChipText(text = "${series.vote_average}", icon = Icons.Filled.Star)
-            }
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                IconButton(onClick = {
-                    if(inWatchList){
-                        viewModel.removeWatchItem(series.id)
-                    }else{
-                        viewModel.insertWatchItem(
-                            WatchItem(
-                                itemId = series.id,
-                                type = false,
-                                series = series
-                            )
-                        )
-                    }
-                    series.in_watchlist = !series.in_watchlist
-                    inWatchList = !inWatchList
-                }) {
-                    Icon(
-                        imageVector = if(inWatchList) Icons.Default.Delete else Icons.Default.AddCircle,
-                        contentDescription = "Add to WatchList"
+        FloatingActionButton(backgroundColor = Color.Red,onClick = {
+            if (inWatchList) {
+                viewModel.removeWatchItem(series.id)
+            } else {
+                viewModel.insertWatchItem(
+                    WatchItem(
+                        itemId = series.id,
+                        type = false,
+                        series = series
                     )
-                }
+                )
             }
+            series.in_watchlist = !series.in_watchlist
+            inWatchList = !inWatchList
+        }) {
+            Icon(
+                imageVector = if (inWatchList) Icons.Default.Delete else Icons.Default.Add,
+                contentDescription = "Add to WatchList",
+                modifier = Modifier.size(32.dp),
+                tint = MaterialTheme.colors.surface
+            )
         }
+    }) {
 
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
         ) {
-            Text(text = series.name, fontSize = 22.sp)
-            Spacer(modifier = Modifier.height(12.dp))
-            ExpandableText(text = series.overview)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(0.7f),
+            ) {
+                series.poster_path?.let {
+                    AsyncImage(
+                        model = "${Constants.IMAGE_BASE}${it}",
+                        contentDescription = "Movie Poster",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.Center
+                    )
+                } ?: Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center)
+                ) {
+                    Text("No Poster Available")
+                }
+                IconButton(
+                    onClick = {
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.padding(12.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.size(48.dp).background(
+                            color = Color.Red,
+                            shape = RoundedCornerShape(6.dp),
+                        ), contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back Arrow",
+                            modifier = Modifier.padding(6.dp),
+                            tint = MaterialTheme.colors.surface
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(20.dp)
+                ) {
+                    ChipText(text = "${series.vote_average}", icon = Icons.Filled.Star)
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .horizontalScroll(rememberScrollState()),
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                series.genre_ids.forEach {
+                    ChipText(text = "${genreDB[it]}")
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                Text(text = series.name, fontSize = 22.sp)
+                Spacer(modifier = Modifier.height(12.dp))
+                ExpandableText(text = series.overview)
+            }
         }
     }
 
